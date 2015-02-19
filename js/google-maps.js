e85e56df27e3f7dbe30b78a0c8e53ff3125c8c93
Nicola Molinari <nicola.molinari@commercetools.de>
2015-02-19 17:47:49 +0100

    Update maps location

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,9 +1,9 @@
 function initialize() {
-      	
-      	var windowLatLng = new google.maps.LatLng(-22.940261, -43.180732);
-	    
+
+      	var windowLatLng = new google.maps.LatLng(52.502541, 13.412209); // betahaus berlin
+
       	var mapCanvas = document.getElementById('map-location');
-      	
+
         var mapOptions = {
 	        center: windowLatLng,
 	        zoom: 13,
@@ -13,36 +13,35 @@ function initialize() {
 			scaleControl: false,
 	        mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-        
+
         var map = new google.maps.Map(mapCanvas, mapOptions);
-        
+
         var contentString = '<div class="info-window">'+
-        	'<h3>Location</h3>'+
+        	'<h3>betahaus Berlin</h3>'+
         	'<span>Where you can find us</span>'+
-        	'<p>20740 Copacabana, Rio de Janeiro RJ 4468, Brazil</p>'+
+        	'<p>Prinzessinnenstr. 19<br>10969 Berlin<br>Germany</p>'+
         	'</div>';
-	
+
 		var infowindow = new google.maps.InfoWindow({
 			content: contentString,
 			maxWidth: 240
 		});
-		
+
         var marker = new google.maps.Marker({
 		    position: windowLatLng,
 		    map: map,
 		    title: 'Hello World!',
 		    show: true
 		});
-		
+
 		google.maps.event.addListener(marker, 'click', function() {
 			infowindow.open(map, marker);
 		});
-		
+
 		google.maps.event.trigger(marker, 'click')
-		
+
 		marker.setVisible(false)
-		
+
       }
-      
+
       google.maps.event.addDomListener(window, 'load', initialize);
-            
