4a909a2b904f61d5d52fb7ca2f46d53a80fc713c
Nicola Molinari <nicola.molinari@commercetools.de>
2015-02-19 17:03:13 +0100

    Remove initial loader

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(function(){
-	
-	
+
+
 	$(window).load(function() {
-		$(".loader").delay(800).fadeOut("slow");
+		$(".loader").delay(500).fadeOut("slow");
 	})
-	
+
 	$window = $(window);
 	if( $window .width() > 800){
 		$('.parallax-scroll1').parallax("50%", 0.5);
@@ -13,19 +13,19 @@ $(function(){
 		$('.parallax-scroll4').parallax("50%", 0.5);
 		$('.parallax-scroll5').parallax("50%", 0.5);
 	}
-	
-	
+
+
 	$('.main-navigation').onePageNav({
-		
+
 		filter: ':not(.external)',
 	    currentClass: 'current',
 		scrollOffset: 85,
 	    scrollSpeed: 1000,
 	    scrollThreshold: 0.5 ,
 	    easing: 'easeInOutExpo'
-	   
+
 	});
-	
+
 	wow = new WOW(
 	    {
 	      boxClass:     'wow',      // default
@@ -35,18 +35,18 @@ $(function(){
 	      live:         true        // default
 	    }
 	);
-	
+
 	wow.init();
-	
+
 	//--- fixed header on scroll
 	var test = 0;
-	
+
 	var $navbar = $('.navbar');
 	var $white_logo_img = $('.white-logo-img');
 	var $dark_logo_img = $('.dark-logo-img');
-	
+
 	function scrolled(test){
-		
+
 		if(test === 0){
 			$navbar.stop().addClass("sticky-navbar");
 			$white_logo_img.fadeOut();
@@ -57,11 +57,11 @@ $(function(){
 			$white_logo_img.fadeIn();
 		}
 	}
-	
+
 	if($(window).scrollTop() > 50){
 		scrolled(test);
 	}
-	
+
 	$(window).scroll(function() {
 		if ($(document).scrollTop() > 50) {
 	    	if(test===0){
@@ -70,27 +70,27 @@ $(function(){
 	    	test=1;
 	    } else {
 	    	if(test===1){
-	        	scrolled(test);        	
+	        	scrolled(test);
 	        }
 	        test = 0;
 	    }
-	}); 
+	});
 	//--- end scroll
-	
+
 	$('.curved-text').circleType({radius:200});
-	
+
 	$('.video-link').magnificPopup({
 		type : 'iframe',
 		closeBtnInside : false,
 		mainClass: 'mfp-fade',
 		removalDelay: 160
 	});
-	
+
 	$("#features-slider").owlCarousel({
 		items : 3,
 		itemsDesktop : [1199,3],
 	});
-	
+
 	$("#team-slider").owlCarousel({
 		items : 3,
 		itemsDesktop : [1199,3],
@@ -98,7 +98,7 @@ $(function(){
 		itemsTablet: [600,1],
 		itemsMobile : false
 	});
-	
+
 	$("#gallery-slider").owlCarousel({
 		singleItem: true,
 		slideSpeed : 400,
@@ -106,12 +106,12 @@ $(function(){
 		pagination: false,
 		navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
 	});
-	
+
 	$("#testi-slider").owlCarousel({
 		singleItem: true,
 		slideSpeed : 400
 	});
-	
+
 	$('#plan-select').selectpicker();
-	
+
 });
